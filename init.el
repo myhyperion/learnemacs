1330f1fb037178ef48a4e6524d6acc5c4bfacf4e
myhyperion <myhyperion@126.com>
2017-02-09 09:16:14 +0800

    little change. Day 2 pre 20 mins

--- init.el
+++ init.el
@@ -7,14 +7,31 @@
 
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(electric-indent-mode -1)
 (global-linum-mode t)
 (setq inhibit-splash-screen t) 
+     
 (defun open-my-init-file()
   (interactive)
   (find-file "~/.emacs.d/init.el"))
-
 (global-set-key (kbd "<f1>") 'open-my-init-file)
+
 (recentf-mode t)
 (global-company-mode t)
-(setq cursor-type 'bar) 
+(setq-default cursor-type 'bar)
+(setq make-backup-files nil) ;; no backup
+
+ (delete-selection-mode t)
+
+  (require 'org)
+(setq org-src-fontify-natively t)
+
+
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 15)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+(global-hl-line-mode t)
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
